21b57a25fd218aab84b1124c7ee03c34795249e1
praneethbandi <prbandi7@outlook.com>
2025-06-01 12:52:19 -0500

    Stuff

---
+++ src/test/java/org/example/springy/SpringyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
